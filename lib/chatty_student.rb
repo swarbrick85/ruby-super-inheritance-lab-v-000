772b84f24f8f437c719b868f429436b83f609c2d
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-08 16:28:58 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,19 +1,10 @@
 class ChattyStudent 
   def hello
-    return "Hey there! I'm so excited to learn stuff. How are you doing today? I'm okay, but I'm kind of tired...., it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+    return "Hey there! I'm so excited to learn stuff. How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
   end 
   
   def raise_hand 
-    return "Pick me!
-    Pick me!
-    Pick me!
-    Pick me!
-    Pick me!
-    Pick me!
-    Pick me!
-    Pick me!
-    Pick me!
-    Pick me!"
+    return "Pick me! Pick me! Pick me! Pick me! Pick me! Pick me! Pick me! Pick me! Pick me! Pick me!"
   end 
   
   
