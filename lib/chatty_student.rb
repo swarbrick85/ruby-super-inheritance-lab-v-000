3efffa9b6700cfa942219e714f5c0a6f70fa9ab3
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-08 16:25:01 +0000

    automatically backed up by learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -1,5 +1,11 @@
 class ChattyStudent 
+  def hello
+    return "Hi, my name is Kimberly! My mom always says that I'm a real live wire."
+  end 
   
+  def raise_hand 
+    return "Pick me! "
+  end 
   
   
   
