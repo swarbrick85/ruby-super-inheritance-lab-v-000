d772aabe227f4e0fc7ca3e962285096eab8563dc
David F Swarbrick Jr <david.swarbrick.jr@gmail.com>
2018-11-08 16:45:27 +0000

    Done.

---
+++ lib/chatty_student.rb
@@ -0,0 +1,18 @@
+class ChattyStudent 
+  def hello
+    puts "Hey there! I'm so excited to learn stuff." 
+    puts "How are you doing today? I'm okay, but I'm kind of tired. Did you watch The Walking Dead last night? You didn't?! Oh man, it was so crazy! What, you don't want any spoilers? Okay well let me just tell you who died..."
+  end 
+  
+  def raise_hand 
+    x = 0 
+    while x < 10 
+    puts "Pick me!"
+    x += 1 
+  end 
+  end 
+  
+  
+  
+  
+end 
